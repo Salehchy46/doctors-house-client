90eceaf0065594d35cf014b3d010f2ef23feea0e
Gygabyte <salehchyctg@gmail.com>
2025-09-22 14:41:35 +0600

    dashboard page is added

---
+++ src/layouts/Dashboard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Dashboard : React.FC = () => {
+    return (
+        <div className='flex'>
+            <div className='w-64 min-h-screen bg-white'>
+                <ul className='menu'></ul>
+            </div>
+            <div></div>
+        </div>
+    );
+};
+
+export default Dashboard;
