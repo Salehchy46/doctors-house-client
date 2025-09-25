80db56735cfc3abd9d260f6f1907af82cb7c0e3a
salehchy46 <salehchyctg@gmail.com>
2025-09-26 01:50:59 +0600

    service section is added with tabs in services

--- src/pages/Home/Reviews.tsx
+++ src/pages/Home/Reviews.tsx
@@ -33,12 +33,44 @@ const Reviews = () => {
         title="What Our Patients Says"
         subTitle="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium..."
       />
-      <div className="py-10">
+      <div className="py-10 invisible md:visible">
         <Swiper
           spaceBetween={50}
           modules={[Navigation]}
           navigation
           slidesPerView={2}
+        >
+          {reviews.map((review, idx) => (
+            <SwiperSlide key={idx}>
+              <div className="card w-[550px] h-[280px] shadow-sm">
+                <div className="card-body">
+                  <div className="flex items-center justify-between mb-4">
+                    <div className="flex items-center">
+                      <div className="mr-3">
+                        <img src={review.image} className="w-16" alt="" />
+                      </div>
+                      <div>
+                        <h2 className="card-title">{review.name}</h2>
+                        <p>{review.review_time}</p>
+                      </div>
+                    </div>
+                    <div>
+                      <img src={quote} className="w-12" alt="" />
+                    </div>
+                  </div>
+                  <p>{review.review}</p>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      <div className="py-3 md:hidden">
+        <Swiper
+          spaceBetween={50}
+          modules={[Navigation]}
+          navigation
+          slidesPerView={1}
         >
           {reviews.map((review, idx) => (
             <SwiperSlide key={idx}>
--- src/pages/Home/Services.tsx
+++ src/pages/Home/Services.tsx
@@ -14,7 +14,7 @@ const Services = () => {
                     <h3 className="text-[40px] font-bold">Our Services</h3>
                     <p className='my-6 text-[16px]'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
                     <Tabs>
-                        <div className='invisible md:visible'>
+                        <div className=''>
                             <TabList className='border-0 flex justify-between'>
                                 <Tab 
                                     className='border-0 rounded-xl bg-[#F7A582] hover:bg-transparent hover:border-[#F7A582]'
@@ -28,8 +28,8 @@ const Services = () => {
                                 </Tab>
                                 <Tab 
                                     className='border-0 rounded-xl bg-[#F7A582] hover:bg-transparent hover:border-[#F7A582]'
-                                    selectedClassName='bg-transparent '>
-                                    <button className='w-[170px] h-[80px] text-[18px]border-2 border-[#F7A582] rounded-xl font-bold'>Oral Surgery</button>
+                                    selectedClassName='bg-transparent border-[#F7A582]'>
+                                    <button className='w-[170px] h-[80px] text-[18px] border-2 border-[#F7A582] rounded-xl font-bold'>Oral Surgery</button>
                                 </Tab>
                             </TabList>
                             <TabPanel>
@@ -51,27 +51,6 @@ const Services = () => {
                                 <img className='card-actions rounded-xl my-6 w-[550px] h-[350px]' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUQJwop_5ohIShPoPDMe89oFmgtxMnvTosXA&s" alt="" />
                                 <h2 className='text-2xl font-bold'>Oral Surgery</h2>
                                 <p className="my-6">Oral surgery is the medical specialty focused on surgically treating diseases and conditions of the mouth, jaw, face, and neck, often involving complex procedures beyond routine dental care. Common procedures include wisdom tooth removal, dental implants, bone and gum grafts, and treatments for facial trauma, oral tumors, and TMJ disorders. Oral and maxillofacial surgeons (OMS) are specialists with dual medical and dental qualifications who bridge the gap between medicine and dentistry.  </p>
-                            </TabPanel>
-                        </div>
-                        <div className='md:hidden flex justify-between rounded-xl'>
-                            <Tab>
-                                <button className='rounded-xl w-[170px] h-[80px] text-[18px] font-bold bg-[#F7A582] hover:bg-transparent hover:border-2 hover:border-[#F7A582]'>Gastrology</button>
-                            </Tab>
-                            <Tab>
-                                <button className='rounded-xl w-[170px] h-[80px] text-[18px] font-bold bg-[#F7A582] hover:bg-transparent hover:border-2 hover:border-[#F7A582]'>Orthopadic</button>
-                            </Tab>
-
-                        </div>
-                        <div className='hidden md:visible'>
-
-                        </div>
-                        <div className='card md:hidden m-6'>
-                            <TabPanel>
-                                <img className='card-actions rounded-xl w-[550px] h-[350px]' src="https://www.nimshospital.com/assets/img/departments/GASTROENTEROLOGY.png" alt="" />
-                                <h3 className="text-2xl font-bold mt-6">Electro  Gastrology Therapy</h3>
-                                <p className='mt-6'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Sed ut perspiciatis unde omnis iste natus error </p>
-                                <p>Sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-
                             </TabPanel>
                         </div>
                     </Tabs>
