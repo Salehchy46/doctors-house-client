5859e681b066a9debfc1d8f9534214f6375b56a7
salehchy46 <salehchyctg@gmail.com>
2025-12-19 10:59:12 +0600

    Ektu UI e vhul hoichilo

--- src/pages/About/Hero.tsx
+++ src/pages/About/Hero.tsx
@@ -5,10 +5,10 @@ const Hero: React.FC = () => {
         <div className="w-full max-w-[1280px] mx-auto bg-gray-50 text-gray-800">
             <div className="bg-teal-950 min-h-[300px] text-white py-20">
                 <div className="max-w-[1280px] mx-auto text-left py-24">
-                    <h1 className="text-4xl font-bold mb-4">Home / About DOC HOUSE</h1>
-                    <p className="text-lg opacity-90">
-                        Caring for your health with compassion, trust, and innovation.
-                    </p>
+                    <p className="">Home / About</p>
+                    <h2 className="text-5xl font-bold opacity-90">
+                        About Doc House
+                    </h2>
                 </div>
             </div>
         </div>
