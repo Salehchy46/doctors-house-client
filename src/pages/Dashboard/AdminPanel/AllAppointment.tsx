d543761cbaa833cc449175ebaeb0e04a55fb73bd
salehchy46 <salehchyctg@gmail.com>
2025-12-26 08:52:25 +0600

    table is gotten

--- src/pages/Dashboard/AdminPanel/AllAppointment.tsx
+++ src/pages/Dashboard/AdminPanel/AllAppointment.tsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+interface Appointment {
+    _id: string;
+    name: string;
+    mobile: string;
+    number: string;
+    time: string;
+    scheduledTime: string;
+}
 
 const AllAppointment: React.FC = () => {
+
+    const [appointments, setAppointments] = useState<Appointment[]>([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/appointments')
+            .then(res => res.json())
+            .then((data: Appointment[]) => setAppointments(data))
+            .catch(error => console.log(error))
+    }, [])
+
     return (
         <div>
-            <h2 className='text-2xl font-bold'>All Appointment : </h2>
+            <h2 className='text-2xl font-bold'>All Appointment : {appointments.length}</h2>
+            <div>
+                {
+                    appointments.map((appointment, index) => <div className="overflow-x-auto">
+                        <table className="table">
+                            {/* head */}
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th>Name</th>
+                                    <th>Phone Number</th>
+                                    <th>Appointment Time</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th>{index}</th>
+                                    <td>{appointment.name}</td>
+                                    <td>Quality Control Specialist</td>
+                                    <td>Blue</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
